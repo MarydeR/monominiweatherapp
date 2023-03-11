f09b3f6d6dba723fa0d4596e12c8e90b1638d333
MarydeR <122215523+MarydeR@users.noreply.github.com>
2023-03-11 13:49:23 +0100

    boostrap container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from "react";
+import Weather from "./Weather.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello MonoMini Weather App</header>
+      <div className="container">
+        <header className="App-header">Hello MonoMini Weather App</header>
+        <Weather />
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Weather.css";
+function WeatherApp() {
+  return (
+    <div className="WeatherApp">
+      <h1>Weather APP</h1>
+    </div>
+  );
+}
+
+export default WeatherApp;
