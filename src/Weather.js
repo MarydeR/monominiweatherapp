c3d474a7dffa29eeb73d46c92bb6271286a20880
MarydeR <122215523+MarydeR@users.noreply.github.com>
2023-03-11 15:10:01 +0100

    main section wireframe

--- src/Weather.js
+++ src/Weather.js
@@ -3,7 +3,47 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <div className="sectionA"></div>
+      <div className="mainsection border border-2 rounded-4">
+        <div className="section1">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="row">
+                <div className="col-md-5">
+                  <div className="maintemperature">
+                    <h2 className="d-inline text-center">XX° </h2>{" "}
+                    <span className="d-inline text-center"> C | F</span>
+                  </div>
+                </div>
+                <div className="col-md-7 iconbox">
+                  <img
+                    src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png"
+                    alt="weatherIcon"
+                  ></img>
+                </div>
+              </div>
+              <div className="row weatherdescription">
+                <ul>
+                  <li> Cloudy</li>
+                  <li> H: xx° </li>
+                  <li> L: xx°</li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-md-6 citybox">
+              <div className="row cityname">
+                <h1>City </h1>
+              </div>
+              <div className="row">
+                <ul>
+                  <li>date</li>
+                  <li>day</li>
+                  <li>local time: hh:mm</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <footer>
         <a
           href="https://github.com/MarydeR/monominiweatherapp"
