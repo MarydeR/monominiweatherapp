89cf04772834417bbed82014bea75d27f2e39ead
MarydeR <122215523+MarydeR@users.noreply.github.com>
2023-03-11 21:00:07 +0100

    loader + axios import

---
+++ src/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <a
+        href="https://github.com/MarydeR/monominiweatherapp"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        OpenSourceCode
+      </a>{" "}
+      by MDR hosted in{" "}
+      <a
+        href="https://quiet-concha-b2732c.netlify.app/"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        Netlify
+      </a>{" "}
+    </footer>
+  );
+}
--- src/Search.js
+++ src/Search.js
@@ -1,52 +1,57 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Weather from "./Weather.js";
+import { Commet } from "react-loading-indicators";
+import Footer from "./Footer.js";
 import "./Search.css";
-import { useState } from "react";
 
 export default function Search() {
-  let city = `Manila`;
-  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=2ec340bdbdo84acaf6ct2a055b44668d&units=metric`;
+  function getTemperature(response) {}
+  let defaultcity = "Manila";
+  let [city, setCity] = useState(defaultcity);
+  const [loaded, setLoaded] = useState(false);
 
-  return (
-    <div className="Search">
-      <div className="mainsection border border-2 rounded-4">
-        <Weather />
-        <div className="row">
-          <form className="row">
-            <div className="col-9">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter City"
-                autocomplete="off"
-              />
-            </div>
-            <div className="col">
-              <button type="submit" className="btn btn-secondary mb-2">
-                Search
-              </button>
-            </div>
-          </form>
+  if (loaded) {
+    return (
+      <div className="Search">
+        <div className="mainsection border border-2 rounded-4">
+          <Weather />
+          <div className="row">
+            <form className="row">
+              <div className="col-9">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter City"
+                  autocomplete="off"
+                />
+              </div>
+              <div className="col">
+                <button type="submit" className="btn btn-secondary mb-2">
+                  Search
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
+        <Footer />
       </div>
-      <footer>
-        <a
-          href="https://github.com/MarydeR/monominiweatherapp"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          OpenSourceCode
-        </a>{" "}
-        by MDR hosted in{" "}
-        <a
-          href="https://quiet-concha-b2732c.netlify.app/"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Netlify
-        </a>{" "}
-      </footer>
-    </div>
-  );
+    );
+  } else {
+    let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=2ec340bdbdo84acaf6ct2a055b44668d&units=metric`;
+    axios.get(apiURL).then(getTemperature);
+
+    return (
+      <div>
+        <div className="Search">
+          <div className="mainsection border border-2 rounded-4">
+            <div>
+              <Commet color="#a4a4a4" size="medium" text="" textColor="" />
+            </div>
+          </div>
+          <Footer />
+        </div>{" "}
+      </div>
+    );
+  }
 }
