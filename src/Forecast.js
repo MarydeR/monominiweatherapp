3f66ca37b423216eb89f6eaa8b3a309c5abce7df
MarydeR <122215523+MarydeR@users.noreply.github.com>
2023-03-18 17:38:42 +0100

    correction of map in forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -32,6 +32,8 @@ export default function Forecast(props) {
                   <ForecastDay data={dailyforecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
